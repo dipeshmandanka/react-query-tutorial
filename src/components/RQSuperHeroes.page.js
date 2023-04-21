d17c1be7047dbb5485e4ab804b967fab21b796c8
dipeshmandanka <dipesh.mandanka@rapidops.com>
2023-04-21 16:40:40 +0530

    till video 10

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,17 +6,16 @@ const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes');
 };
 export default function RQSuperHeroesPage() {
-    const { data, isLoading, isError, error, isFetching } = useQuery(
+    const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
         'super-heroes',
         fetchSuperHeroes,
         {
-            refetchInterval: 10000, // default false
-            refetchIntervalInBackground: true // default false
+            enabled: false,
         }
     );
-
+        
     console.log({ isFetching, isLoading });
-    if (isLoading) {
+    if (isLoading || isFetching) {
         return <h2>Loading...</h2>;
     }
 
@@ -27,6 +26,7 @@ export default function RQSuperHeroesPage() {
     return (
         <div>
             <h2>RQ SuperHeroes</h2>
+            <button onClick={refetch}>Fetch Data</button>
             {data?.data.map(hero => (
                 <div key={hero.id}>{hero.name}</div>
             ))}
