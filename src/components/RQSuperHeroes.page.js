4ecbdf28380367f4205eec6f4d320e6d80a0fe34
dipeshmandanka <dipesh.mandanka@rapidops.com>
2023-04-21 15:44:28 +0530

    till video 7

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,8 +6,11 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes")
 }
 export default function RQSuperHeroesPage() {
-  const {data, isLoading, isError, error} = useQuery("super-heroes", fetchSuperHeroes);
+  const {data, isLoading, isError, error, isFetching} = useQuery("super-heroes", fetchSuperHeroes, {
+    staleTime: 30000
+  });
   
+  console.log({isFetching, isLoading});
   if (isLoading) {
     return <h2>Loading...</h2>
   }
