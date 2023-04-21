d71964a6e00a328a6bbcd396d8b487752546c43e
dipeshmandanka <dipesh.mandanka@rapidops.com>
2023-04-21 16:18:43 +0530

    till video 8

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,28 +1,35 @@
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes")
-}
+    return axios.get('http://localhost:4000/superheroes');
+};
 export default function RQSuperHeroesPage() {
-  const {data, isLoading, isError, error, isFetching} = useQuery("super-heroes", fetchSuperHeroes, {
-    staleTime: 30000
-  });
-  
-  console.log({isFetching, isLoading});
-  if (isLoading) {
-    return <h2>Loading...</h2>
-  }
+    const { data, isLoading, isError, error, isFetching } = useQuery(
+        'super-heroes',
+        fetchSuperHeroes,
+        {
+            refetchOnMount: true,
+            refetchOnWindowFocus: true
+        }
+    );
 
-  if (isError) {
-    return <h2>{error.message}</h2>
-  }
-  
-  return (
-    <div>
-      <h2>RQ SuperHeroes</h2>
-      {data?.data.map(hero => <div key={hero.id}>{hero.name}</div>)}
-    </div>
-  )
+    console.log({ isFetching, isLoading });
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+
+    if (isError) {
+        return <h2>{error.message}</h2>;
+    }
+
+    return (
+        <div>
+            <h2>RQ SuperHeroes</h2>
+            {data?.data.map(hero => (
+                <div key={hero.id}>{hero.name}</div>
+            ))}
+        </div>
+    );
 }
